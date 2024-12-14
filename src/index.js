8b84144331dd93c6c1f3ac68b43f87caad7a2aed
Jairo Calcina <j.calcina.v@gmail.com>
2024-12-14 22:58:44 +0000

    feat: add main function to start the server

---
+++ src/index.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const grpc = require("@grpc/grpc-js");
+const createServer = require("./server");
+const { connectMongo, closeMongo } = require("./db/mongoClient");
+const { GRPC_SERVER_ADDRESS } = require("./config");
+
+(async function main() {
+  try {
+    await connectMongo();
+    const server = createServer();
+    console.log(server);
+    server.bindAsync(
+      GRPC_SERVER_ADDRESS,
+      grpc.ServerCredentials.createInsecure(),
+      (err, port) => {
+        if (err) {
+          console.error("Failed to bind gRPC server:", err);
+          process.exit(1);
+        }
+        console.log(
+          "CareerService gRPC server is listening on",
+          GRPC_SERVER_ADDRESS
+        );
+      }
+    );
+
+    process.on("SIGINT", async () => {
+      console.log("Shutting down gRPC server");
+      await closeMongo();
+      process.exit(0);
+    });
+  } catch (err) {
+    console.error("Error starting server:", err);
+    process.exit(1);
+  }
+})();
