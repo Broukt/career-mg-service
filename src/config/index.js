00b129b697ea498cb307b332ad2e3ef7dc5251a0
Jairo Calcina <j.calcina.v@gmail.com>
2024-12-14 19:51:42 +0000

    feat: add configuration variables

---
+++ src/config/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  MONGO_URI: process.env.MONGO_URI,
+  DATABASE_NAME: process.env.DATABASE_NAME,
+  COLLECTION_NAME: process.env.COLLECTION_NAME,
+  GRPC_SERVER_ADDRESS: process.env.GRPC_SERVER_ADDRESS,
+};
