390d09ed23727bede6b027e37cd040ec6f95a0fb
Jairo Calcina <j.calcina.v@gmail.com>
2024-12-15 15:20:07 +0000

    refactor: change collection name

--- src/repositories/careersRepository.js
+++ src/repositories/careersRepository.js
@@ -1,10 +1,9 @@
 const { getDb } = require("../db/mongoClient");
-const { COLLECTION_NAME } = require("../config");
 const { ObjectId } = require("mongodb");
 
 async function getAllCareers() {
   const db = await getDb();
-  return db.collection(COLLECTION_NAME).find().toArray();
+  return db.collection('careers').find().toArray();
 }
 
 module.exports = {
